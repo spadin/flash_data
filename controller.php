05618a70795337a28191ae014a929067014ceb77
Sandro Padin <sandropadin@gmail.com>
2011-12-27 17:36:41 -0600

    Bumping version to 0.9.2

--- controller.php
+++ controller.php
@@ -5,7 +5,7 @@ class FlashDataPackage extends Package {
   
   protected $pkgHandle = 'flash_data';
   protected $appVersionRequired = '5.4.2.2';
-  protected $pkgVersion = '0.9.1';
+  protected $pkgVersion = '0.9.2';
 
   public function getPackageDescription() {
     return t("Set and retrieve flash data. Similar to Rails flash messages");
